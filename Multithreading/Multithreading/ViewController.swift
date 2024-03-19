512406f624ee89dbd308b365a73d16d35ad73040
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-20 02:29:36 +0500

    Task complete

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var name = "I love RM"
+    var lock = NSLock()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateName()
     }
-
-
+    
+    func updateName() {
+        DispatchQueue.global().async {
+            self.lock.lock()
+            print(self.name) // Считываем имя из global
+            print(Thread.current)
+            self.lock.unlock()
+        }
+        
+        print(self.name) // Считываем имя из main
+    }
+    
 }
 
+/*
+ В данном примере проблема с гонкой данных (Data Race), вызванная ленивой переменной.
+ */
