10963feec17091f6af197a032732efc5e425bf9f
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-19 23:38:44 +0500

    Task init

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var name = "Введите имя"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateName()
     }
-
-
+    
+    func updateName() {
+        DispatchQueue.global().sync {
+            self.name = "I love RM" // Перезаписываем имя в другом потоке
+            print(Thread.current)
+            print(self.name)
+        }
+        
+        print(self.name) // Считываем имя из main
+    }
 }
-
