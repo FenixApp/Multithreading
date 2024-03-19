615458bf21bb0a0aee797c326da1a12c94f5ba0b
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-19 12:30:51 +0500

    Task complete

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Thread.detachNewThread {
+            for _ in (0..<10) {
+                let currentThread = Thread.current
+                print("1, Current thread: \(currentThread)")
+            }
+        }
+        
+        for _ in (0..<10) {
+            let currentThread = Thread.current
+            print("2, Current thread: \(currentThread)")
+        }
     }
-
-
+    /// Разгрузить главный поток. Только первый цикл перевести в другой поток с помощью Thread.detachNewThread и объяснить почему изменился вывод.
+    /*
+     Изменился вывод, потому что написанный код разделяет выполнение циклов в двух разных потоках.
+     Первый цикл работает в отдельном потоке, который создан с помощью detachNewThread(), а второй цикл выполняется в главном потоке.
+     */
 }
 
