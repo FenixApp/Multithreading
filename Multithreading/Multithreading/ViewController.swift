9382cae3aaf20cd515771dd568adba4a6940575f
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-22 02:59:02 +0500

    Task complete

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    final class Post: @unchecked Sendable {
+        
+    }
+    
+    enum State1: Sendable {
+        case loading
+        case data(String)
+    }
+    
+    enum State2: Sendable {
+        case loading
+        case data(Post) // Out: Associated value 'data' of 'Sendable'-conforming enum 'State2' has non-sendable type 'ViewController.Post'
+    }
 }
 
