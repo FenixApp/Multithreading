02d08a38992e2b7a534f91e2fc58a5fe8035f91e
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-21 23:35:06 +0500

    Task complete

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let service = ArrayAdditionService()
+        for i in 1...10 {
+            service.addElement(i)
+        }
+        service.cancelAddition()
     }
-
-
+    
 }
 
+// Класс, представляющий сервис операций добавления в массив
+class ArrayAdditionService {
+    private var array = [Int]()
+    private var pendingWorkItems = [DispatchWorkItem]()
+    
+    // Метод для добавления элемента в массив
+    func addElement(_ element: Int) {
+        // Создаем новую операцию для добавления элемента в массив
+        let newWorkItem = DispatchWorkItem { [weak self] in
+            self?.array.append(element)
+            print("Элемент \(element) успешно добавлен в массив.")
+        }
+        
+        // Сохраняем новую операцию
+        pendingWorkItems.append(newWorkItem)
+        
+        // Даем пользователю время для отмены операции
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            // Логика тут eсли операция не была отменена, выполняем ее
+            newWorkItem.perform()
+        }
+    }
+    
+    // Метод для отмены операции добавления элемента в массив
+    func cancelAddition() {
+        guard let lastWorkItem = pendingWorkItems.last else {
+            print("Нет операций для отмены.")
+            return
+        }
+        
+        // Тут отменяем последнюю операцию
+        lastWorkItem.cancel()
+    }
+}
