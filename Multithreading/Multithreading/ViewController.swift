a38b467547be547c1dd56347e9adc39c845a754e
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-21 20:50:27 +0500

    Task complete

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let phraseSemaphore = DispatchSemaphore(value: 1)
+//        
+//        DispatchQueue.global().async {
+//            let phrasesService = PhrasesService()
+//            
+//            for i in 0..<10 {
+//                phraseSemaphore.wait()
+//                phrasesService.addPhrase("Phrase \(i)")
+//                phraseSemaphore.signal()
+//            }
+//            
+//            // Даем потокам время на завершение работы
+//            Thread.sleep(forTimeInterval: 1)
+//            
+//            // Выводим результат
+//            print(phrasesService.phrases)
+//            
+//            print(Thread.current)
+//            print(phraseSemaphore.description)
+//        }
+        
+        let phrasesService = PhrasesService()
+        
+        DispatchQueue.global().async {
+            for i in 0..<10 {
+                phrasesService.addPhrase("Phrase \(i)")
+            }
+        }
+        
+        // Даем потокам время на завершение работы
+        Thread.sleep(forTimeInterval: 1)
+        
+        // Выводим результат
+        print(phrasesService.phrases)
+        
     }
-
-
+    
+    actor PhrasesService {
+        var phrases: [String] = []
+        
+        func addPhrase(_ phrase: String) {
+            phrases.append(phrase)
+        }
+    }
 }
 
+//class PhrasesService {
+//    var phrases: [String] = []
+//    
+//    func addPhrase(_ phrase: String) {
+//        phrases.append(phrase)
+//    }
+//}
