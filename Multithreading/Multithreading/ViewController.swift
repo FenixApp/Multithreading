f8b8d9473c6fa987fd93f35532ef4d341ebddcb1
Dmitriy Starozhilov <dimstar-90@mail.ru>
2024-03-22 09:24:34 +0500

    Task complete

--- Multithreading/Multithreading/ViewController.swift
+++ Multithreading/Multithreading/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
+/// Проблема - гонка данных
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Использование
+        let threadSafeArray = ThreadSafeArray()
+        let operationQueue = OperationQueue()
+        
+        let firstOperation = FirstOperation(threadSafeArray: threadSafeArray)
+        let secondOperation = SecondOperation(threadSafeArray: threadSafeArray)
+        
+        operationQueue.addOperation(firstOperation)
+        secondOperation.addDependency(firstOperation)
+        operationQueue.addOperation(secondOperation)
+        
+        // Дождитесь завершения операций перед выводом содержимого массива
+        operationQueue.waitUntilAllOperationsAreFinished()
+        
+        print(threadSafeArray.getAll())
     }
-
-
+    
+    
 }
 
+// Объявляем класс для для синхронизации потоков
+class ThreadSafeArray {
+    private var array: [String] = []
+    
+    func append(_ item: String) {
+        array.append(item)
+    }
+    
+    func getAll() -> [String] {
+        return array
+    }
+}
+
+// Определяем первую операцию для добавления строки в массив
+class FirstOperation: Operation {
+    let threadSafeArray: ThreadSafeArray
+    
+    init(threadSafeArray: ThreadSafeArray) {
+        self.threadSafeArray = threadSafeArray
+    }
+    
+    override func main() {
+        if isCancelled { return }
+        threadSafeArray.append("Первая операция")
+    }
+}
+
+// Определяем вторую операцию для добавления строки в массив
+class SecondOperation: Operation {
+    // Создаем по образу первой операции
+    let threadSafeArray: ThreadSafeArray
+    
+    init(threadSafeArray: ThreadSafeArray) {
+        self.threadSafeArray = threadSafeArray
+    }
+    
+    override func main() {
+        if isCancelled { return }
+        threadSafeArray.append("Вторая операция")
+    }
+}
